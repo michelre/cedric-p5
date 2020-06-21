a7f2c013ca88812ddf142fa2dd55d2e392aa164b
cedricchaffard <cedric.chaffard@gmail.com>
2020-06-21 15:05:03 +0200

    Parrallax FX / Scroll down / Clouds improvements - Account / Checkout... files added

--- js/script.js
+++ js/script.js
@@ -15,29 +15,8 @@ cercle.addEventListener('click', () => {
 });
 
 
-//let i = 0; // Start point
-//let images = [];
-//let time = 500;
-//
-//
-//// Image List
-//images[0] = "img/teddy_1.jpg";
-//images[1] = "img/teddy_2.jpg";
-//images[2] = "img/teddy_3.jpg";
-//images[3] = "img/teddy_4.jpg";
-//images[4] = "img/teddy_5.jpg";
-//
-//// Change images
-//function changeImg(){
-//    document.slide.src = images[i];
-//
-//    if(i < images.length - 1){
-//        i++;
-//    }else {
-//        i = 0;
-//    }
-//
-//    setTimeout("changeImg()", time);
-//}
-//
-//window.onload = changeImg;
+document.addEventListener('DOMContentLoaded', function() {
+    window.onscroll = function(ev) {
+        document.getElementById('scrolldown').className = (window.pageYOffset < 20) ? "scrolldown-invisible" : "scrolldownVisible";
+    };
+});
